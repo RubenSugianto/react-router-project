88afdd83c8f3957ca9346ac9e77f36c2111b3f78
RubenSugianto <ruben.sugianto@gmail.com>
2025-04-02 22:08:18 +0700

    Where Should loader() Code Be Stored? & When Are loader() Functions Executed? & Reflecting The Current Navigation State in the UI

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,14 +1,17 @@
-import { Outlet, useLoaderData } from "react-router-dom";
+import { Outlet, useLoaderData, useNavigation } from "react-router-dom";
 
 import MainNavigation from '../components/MainNavigation';
 
 function RootLayout() {
     // const events = useLoaderData();
     // console.log(events);
+    // const navigation = useNavigation();
+
     return (
         <>
             <MainNavigation />
             <main>
+                {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
                 <Outlet />
             </main>
         </>
