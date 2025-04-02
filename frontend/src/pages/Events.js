aff39bb1a9b6676c224a1e778b321c42ebee9fcb
RubenSugianto <ruben.sugianto@gmail.com>
2025-04-02 22:28:16 +0700

    Error Handling with Custom Errors

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -6,16 +6,21 @@ function EventsPage() {
     const data = useLoaderData();
     const events = data.events;
 
+    // if (data.isError) {
+    //     return <div>Error: {data.message}</div>;
+    // }
+
     return <EventsList events={events} />;
 }
 
 export default EventsPage;
 
 export async function loader() {
-    const response = await fetch('http://localhost:8080/events');
+    const response = await fetch('http://localhost:8080/event');
 
     if (!response.ok) {
-        // ...
+        // return { isError: true, message: 'Could not fetch events.'}
+        throw { message: 'Could not fetch events.' };
     } else {
         return response;
     }
