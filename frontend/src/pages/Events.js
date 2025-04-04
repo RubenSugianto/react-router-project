17915ba289dd3dd39b6f4ef6d59a5ccb0f186264
RubenSugianto <ruben.sugianto@gmail.com>
2025-04-04 21:40:24 +0700

    Deferring Data Fetching with defer()

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,21 +1,33 @@
-import { useLoaderData, json } from 'react-router-dom';
+import { useLoaderData, Await } from 'react-router-dom';
 
 import EventsList from '../components/EventsList';
+import { Suspense } from 'react';
 
 function EventsPage() {
-    const data = useLoaderData();
-    const events = data.events;
+    // const data = useLoaderData();
+    // const events = data.events;
+
+    // return <EventsList events={events} />;
 
     // if (data.isError) {
     //     return <div>Error: {data.message}</div>;
     // }
 
-    return <EventsList events={events} />;
+    const {events} = useLoaderData();
+
+    return (
+        <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+            <Await resolve={events}>
+                {(loadedEvents) => <EventsList events={loadedEvents} />}
+            </Await>
+        </Suspense>
+    
+    );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
     const response = await fetch('http://localhost:8080/events');
 
     if (!response.ok) {
@@ -32,6 +44,14 @@ export async function loader() {
         //     }
         // );
     } else {
-        return response;
+        // return response;
+        const resData = await response.json();
+        return resData.events;
     }
+}
+
+export function loader() {
+    return {
+        events: loadEvents(),
+    };
 }
